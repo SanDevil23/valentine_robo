f30cb02d028b606e968f0235d4d03dd9c55c7825
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2026-02-16 23:17:34 +0530

    generate code with openai client

--- internal/llm/openaiClient.go
+++ internal/llm/openaiClient.go
@@ -1,6 +1,7 @@
 package llm
 
 import (
+	"context"
 	"os"
 
 	openai "github.com/sashabaranov/go-openai"
@@ -14,4 +15,42 @@ func GetNewClient() *Client  {
 	return &Client{
 		client: openai.NewClient(os.Getenv("OPENAI_API_KEY")),
 	}
+}
+
+func (cl *Client) GenerateCode(ctx context.Context, requirement string) (string, error) {
+	prompt := `You are a senior Golang engineer. Generate a production-ready project for:` + requirement + `Output ONLY in this JSON format:
+				{
+				"files": [
+				{
+					"path": "file path here",
+					"content": "code here"
+				}
+				]
+				}
+	`
+
+	resp, err := cl.client.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4oMini, // fast & cheap
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    "system",
+					Content: "You are an expert backend engineer",
+				},
+				{
+					Role:    "user",
+					Content: prompt,
+				},
+			},
+			Temperature: 0.2,
+		},
+	)
+
+	if err != nil {
+		return "Failed to generate code", err
+	}
+
+	return resp.Choices[0].Message.Content, nil
+
 }

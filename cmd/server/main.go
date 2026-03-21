9cf12c427ae73493a167b3eabacfb34ed3958863
SanDevil23 <sankalpadhya05@gmail.com>
2026-03-22 01:44:38 +0530

    adding home controller

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,14 +6,24 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/sandevil23/valentin_robo/internal/commands"
+	// "github.com/sandevil23/valentin_robo/internal/commands"
+	"github.com/joho/godotenv"
+	"github.com/sandevil23/valentin_robo/internal/controllers"
 	"github.com/sandevil23/valentin_robo/internal/webhook"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err!=nil{
+		log.Fatalln("Failed to load environment variables")
+	}
+
+	// routes
 	http.HandleFunc("/webhook", webhook.Handle)
+	http.HandleFunc("/", controllers.HomeHandler)
+
 	log.Println("Valentine Robo server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
-	commands.IsGenerateCommand("/generate list APIs");
+	// commands.IsGenerateCommand("/generate list APIs");
 }
